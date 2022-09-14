85319fab41b0cfe372cafceb2759e0573d74eca4
AagundisR <agundisRikolino@gmail.com>
2022-09-13 23:24:57 -0500

    Initial commit

---
+++ src/main/java/me/damiannylon/damiannyloninventoryrecover/PlayerDeathListener.java
@@ -0,0 +1,12 @@
+package me.damiannylon.damiannyloninventoryrecover;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class PlayerDeathListener implements Listener {
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent e){
+        
+    }
+}
